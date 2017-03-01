30d43e4cf1c0b9f2731c3e5c8b6805c89f284d25
arfullight <tmimvhuang@gmail.com>
2017-03-01 17:03:48 +0800

    add progress bar

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem 'nokogiri'
 gem 'pry'
+gem 'progress_bar'
--- ioh-talks-crawler.rb
+++ ioh-talks-crawler.rb
@@ -2,6 +2,7 @@ require 'nokogiri'
 require 'pry'
 require 'json'
 require 'open-uri'
+require 'progress_bar'
 
 talks = []
 
@@ -9,6 +10,9 @@ home = "http://ioh.tw"
 
 url = 'http://ioh.tw/talks'
 doc = Nokogiri::HTML(open(url))
+
+totalPage = 44
+bar = ProgressBar.new(totalPage)
 
 # page = "http://ioh.tw/talks/page/#{num}"
 # IOH got talk pages 2 ~ 38, update to 2016 / 11 / 05 (Sat)
@@ -19,7 +23,6 @@ doc = Nokogiri::HTML(open(url))
   # doc = Nokogiri::HTML(str.encode("utf-8", :invalid => :replace, :undef => :replace))
 
 doc.css('div#main div.row-fluid article').each do |talks_info|
-
   page_one = {
     url: "#{home}" + talks_info.css('a')[0][:href],
     name: talks_info.css('h4/a')[0].text,
@@ -28,24 +31,23 @@ doc.css('div#main div.row-fluid article').each do |talks_info|
     department: talks_info.css('.category/a')[0].text,
     description: talks_info.css('p')[0].text.gsub("\n",''),
   }
-  
+
   if talks_info.css('div.schools/a')[1] != nil
     page_one[:school] = [talks_info.css('div.schools/a')[0].text, talks_info.css('div.schools/a')[1].text]
   else
     page_one[:school] = [talks_info.css('div.schools/a')[0].text]
-  end 
-  
-  talks << page_one
+  end
 
+  talks << page_one
 end
 
-(2..38).each do |num|
+bar.increment!
 
+(2..44).each do |num|
   page = "http://ioh.tw/talks/page/#{num}"
   doc_page = Nokogiri::HTML(open(page))
 
   doc_page.css('div#main div.row-fluid article').each do |talks_info|
-
     if talks_info.css('a')[0] != nil
       data = {
         url: "#{home}" + talks_info.css('a')[0][:href],
@@ -60,13 +62,13 @@ end
         data[:school] = [talks_info.css('div.schools/a')[0].text, talks_info.css('div.schools/a')[1].text]
       else
         data[:school] = [talks_info.css('div.schools/a')[0].text]
-      end 
+      end
     end
 
     talks << data
-
   end
 
+  bar.increment!
 end
 
 File.open('ioh-talks-for-ncnu.json', 'w') {|file| file.write(JSON.pretty_generate(talks))}
